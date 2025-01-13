d256c6e424c530ef06570d7de5e7ed883956cb79
Jreng7 <jr.waydev@gmail.com>
2025-01-13 08:16:11 -0300

    remove font

--- dashboard-modern/src/app/layout.tsx
+++ dashboard-modern/src/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { cn } from '@/lib/utils'
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,10 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={cn(
+        "min-h-screen bg-background font-sans antialiased"
+      )}> 
+      {children}
       </body>
     </html>
   );
