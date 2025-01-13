66c4e66c5b4a5b4fa13aff5b4714db7883b5d2b0
Jreng7 <jr.waydev@gmail.com>
2025-01-13 19:18:56 -0300

    atualizações modo desktop

--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { cn } from '@/lib/utils'
+import { Sidebar } from '@/components/sidebar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         "min-h-screen bg-background font-sans antialiased",
         inter.className
       )}>
+        <Sidebar />
         {children}
       </body>
     </html>
