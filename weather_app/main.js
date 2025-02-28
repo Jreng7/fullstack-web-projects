07dbacef376fd4f2d6f5ccf09a09fd3f51fe8ac5
Jreng7 <jr.waydev@gmail.com>
2025-02-28 12:47:03 -0300

    estruturando querySelector

--- weather_app/main.js
+++ weather_app/main.js
@@ -1,7 +1,17 @@
 
 const apiKey = '1754c7325aa39d03e97352b3f57cdd54'
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=berlin'
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=varginha'
 
 async function checarClima(){
+
   const response = await fetch(apiUrl + `&appid=${apiKey}`)
+  let data = await response.json()
+
+  console.log(data)
+
+  document.querySelector('.cidade').innerHTML = data.name
+
+  
 }
+
+checarClima()
