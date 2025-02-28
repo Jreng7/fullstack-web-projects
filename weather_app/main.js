12344be0f567fbfc6612cbd8a00cf61592eccc7b
Jreng7 <jr.waydev@gmail.com>
2025-02-28 09:03:45 -0300

    response await fetch (apenas juntou url com a key

--- weather_app/main.js
+++ weather_app/main.js
@@ -3,5 +3,5 @@ const apiKey = '1754c7325aa39d03e97352b3f57cdd54'
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=berlin'
 
 async function checarClima(){
-  
+  const response = await fetch(apiUrl + `&appid=${apiKey}`)
 }
