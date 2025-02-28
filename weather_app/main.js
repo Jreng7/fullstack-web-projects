4f7bb17cd1ef7e2ba3cec1da093ecea531fd4697
Jreng7 <jr.waydev@gmail.com>
2025-02-28 08:56:28 -0300

    trabalhando no javascript

---
+++ weather_app/main.js
@@ -0,0 +1,4 @@
+https://api.openweathermap.org/data/2.5/weather?q=varginha&appid=1754c7325aa39d03e97352b3f57cdd54&units=metric
+
+const apiKey = ''
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=varginha&appid=1754c7325aa39d03e97352b3f57cdd54&units=metric'
