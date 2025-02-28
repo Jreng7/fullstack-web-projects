3902fd6fa8bfa5b568e28fdfaf99d404cac99b3a
Jreng7 <jr.waydev@gmail.com>
2025-02-28 17:52:09 -0300

    ajustando erros

--- weather_app/main.js
+++ weather_app/main.js
@@ -1,4 +1,3 @@
-
 const apiKey = '1754c7325aa39d03e97352b3f57cdd54'
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q='
 
@@ -8,10 +7,10 @@ const climaTempoIcon = document.querySelector('.clima-icon')
 
 
 async function checarClima(cidadeNome, estadoNome, pais){
-
+  
   const response = await fetch(`${apiUrl}${cidadeNome},${estadoNome},${pais}&appid=${apiKey}`)
 
-  if ( response.status == 404 ){
+  if (response.status == 404) {
 
     document.querySelector('.error').style.display = 'block'
     document.querySelector('.clima').style.display = 'none'
@@ -20,40 +19,46 @@ async function checarClima(cidadeNome, estadoNome, pais){
 
     let data = await response.json()
 
-    if( data.weather[0].main == 'Clouds' ){
+    document.querySelector('.clima').style.display = 'block'
+
+    document.querySelector('.cidade').innerHTML = data.name + ' -'
+    document.querySelector('.temperatura').innerHTML = Math.round(data.main.temp) + ' °C'
+    document.querySelector('.umidade').innerHTML = data.main.humidity + '%'
+    document.querySelector('.vento').innerHTML = data.wind.speed + ' km/h'
+    document.querySelector('.estado').innerHTML = estadoNome
+
+    if (data.weather[0].main == 'Clouds') {
       climaTempoIcon.src = 'images/nublado.png'
-    } else if ( data.weather[0].main == 'Clear' ){
+    } else if (data.weather[0].main == 'Clear') {
       climaTempoIcon.src = 'images/sol.png'
-    } else if ( data.weather[0].main == 'Rain' ) {
+    } else if (data.weather[0].main == 'Rain') {
       climaTempoIcon.src = 'images/chuva.png'
-    } else if ( data.weather[0].main == 'Drizzle' ){
+    } else if (data.weather[0].main == 'Drizzle') {
       climaTempoIcon.src = 'images/chuvisco.png'
-    } else if ( data.weather[0].main == 'Mist' ) {
+    } else if (data.weather[0].main == 'Mist') {
       climaTempoIcon.src = 'images/neblina.png'
     }
   }
-
-
-  document.querySelector('.cidade').innerHTML = data.name + ' -'
-  document.querySelector('.temperatura').innerHTML = Math.round(data.main.temp) + ' °C'
-  document.querySelector('.umidade').innerHTML = data.main.humidity + '%'
-  document.querySelector('.vento').innerHTML = data.wind.speed + ' km/h'
-  document.querySelector('.estado').innerHTML = estadoNome
-
-  document.querySelector('.clima').style.display = 'block'
-
 }
 
+botaoPesquisa.addEventListener('click', () => {
 
-botaoPesquisa.addEventListener('click', ()  => {
+  let entradaCidade = cidadeEstado.value
 
-  let entradaCidade = cidadeEstado.value.trim()
-  let partesArrayString = entradaCidade.split(',')
+  if (entradaCidade) {
 
-  let cidadeNome = partesArrayString[0].trim()
-  let estadoNome = partesArrayString[1].trim()
-  let pais = partesArrayString[2].trim()
+    let partesArrayString = entradaCidade.split(',')
 
-  checarClima(cidadeNome, estadoNome, pais)
+    if (partesArrayString.length === 3) {
+      let cidadeNome = partesArrayString[0].trim()
+      let estadoNome = partesArrayString[1].trim()
+      let pais = partesArrayString[2].trim()
 
+      checarClima(cidadeNome, estadoNome, pais)
+
+    } else {
+      document.querySelector('.error').style.display = 'block'
+      document.querySelector('.clima').style.display = 'none'
+    }
+  }
 })
